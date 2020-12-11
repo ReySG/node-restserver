e22007accec044e4f99de8e79f49c7d2746a46c7
ReySG <reinaldox99@gmail.com>
2020-12-11 03:07:28 -0300

    Subida de archivos

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -42,8 +42,34 @@ let verificaAdmin_Role = (req, res, next) => {
     }
 }
 
+//=============
+//Verificar Token IMG
+//=============
+
+let verificaTokenImg = (req, res, next) => { 
+
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: 'Token Inválido'
+            });
+        }
+        req.usuario = decoded.usuario;
+        next();
+
+    });
+
+
+
+}
+
 
 module.exports = {
     verificaToken,
-    verificaAdmin_Role
+    verificaAdmin_Role,
+    verificaTokenImg
 }
